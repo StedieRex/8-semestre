2e883e715ff0965acebfeb929d28f3b194903e53
Luis Pach <luis.pachoe@alumno.buap.mx>
2024-10-08 14:59:29 -0600

    sistemasDist-chat

---
+++ prograDistribuida/1/algorimosEleccion/PaqueteGloton/src/paquetegloton/Item.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquetegloton;
+
+/**
+ *
+ * @author luisb
+ */
+public class Item {
+    int peso;
+    int valor;
+
+    public Item(int peso, int valor){
+        this.peso = peso;
+        this.valor = valor;
+    }    
+}
---
+++ prograDistribuida/2/1-chat/Cifrado.java
@@ -0,0 +1,56 @@
+public class Cifrado implements java.io.Serializable{
+    private final String alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 áéíóú";
+    private int busca_posicion(char letra){
+        int i=0;
+        boolean encontrado = false;
+        while((i<alfabeto.length())&&(!encontrado)){
+            if(alfabeto.charAt(i)==letra){
+                encontrado = true;
+            }else{
+                i++;
+            }
+        }
+
+        if (encontrado){
+            return i;
+        }else{
+            return -1;
+        }
+    }
+
+    public String Cifrar(String cadena, int llave){
+        int posicion,nueva_posicion;
+        String cadena_cifrada = "";
+        char letra;
+
+        for(int i=0;i<cadena.length();i++){
+            posicion = busca_posicion(cadena.charAt(i));
+            if(posicion!=-1){
+                nueva_posicion = (posicion+llave)%alfabeto.length();
+                letra = alfabeto.charAt(nueva_posicion);
+            }else{
+                letra = cadena.charAt(i);
+            }
+            cadena_cifrada = cadena_cifrada + letra;
+        }
+        return cadena_cifrada;
+    }
+
+    public String Descifrar(String cadena, int llave){
+        int posicion, nueva_posicion;
+        String cadena_descifrada = "";
+        char letra;
+
+        for(int i=0;i<cadena.length(); i++){
+            posicion = busca_posicion(cadena.charAt(i));
+            if(posicion!=-1){
+                nueva_posicion = (posicion+alfabeto.length()-llave)%alfabeto.length();
+                letra = alfabeto.charAt(nueva_posicion);
+            }else{
+                letra = cadena.charAt(i);
+            }
+            cadena_descifrada = cadena_descifrada + letra;
+        }
+        return cadena_descifrada;
+    }
+}
